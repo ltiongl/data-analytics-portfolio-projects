5fb1d57358ae66c6f2249acae6139c1d7a2ea455
Lily TIONG <73985806+ltiongl@users.noreply.github.com>
2024-10-14 13:46:41 +0900

    Update data_exploration.sql

--- ecommerce-electronics-store-sales/data_exploration.sql
+++ ecommerce-electronics-store-sales/data_exploration.sql
@@ -13,8 +13,8 @@ LIMIT 100;
 -- Total revenue by month
 
 SELECT 
-	DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
-	ROUND(SUM(price), 2) AS total_revenue
+    DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
+    ROUND(SUM(price), 2) AS total_revenue
 FROM `events_updated`
 WHERE `event_type` = 'purchase'
 GROUP BY `year-month`
@@ -26,9 +26,9 @@ ORDER BY total_revenue DESC;
 -- Total revenue by month and category
 
 SELECT 
-	DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
+    DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
     `category`,
-	ROUND(SUM(price), 2) AS total_revenue
+    ROUND(SUM(price), 2) AS total_revenue
 FROM `events_updated`
 WHERE `event_type` = 'purchase'
 GROUP BY `year-month`, `category`
@@ -39,8 +39,8 @@ ORDER BY `year-month`, total_revenue DESC;
 -- Total revenue by hour
 
 SELECT 
-	HOUR(`event_time`) AS `hour`,
-	ROUND(SUM(price), 2) AS total_revenue
+    HOUR(`event_time`) AS `hour`,
+    ROUND(SUM(price), 2) AS total_revenue
 FROM `events_updated`
 WHERE `event_type` = 'purchase'
 GROUP BY `hour`
@@ -51,36 +51,36 @@ ORDER BY total_revenue DESC;
 -- Total revenue by hour for each month
 
 WITH cte_ranking AS (
-	SELECT
-		DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
-		HOUR(`event_time`) AS `hour`,
-		ROUND(SUM(price), 2) AS total_revenue,
-		RANK() OVER (PARTITION BY DATE_FORMAT(`event_time`, '%Y-%m') ORDER BY SUM(price) DESC) AS rank_desc,
+    SELECT
+        DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
+        HOUR(`event_time`) AS `hour`,
+        ROUND(SUM(price), 2) AS total_revenue,
+        RANK() OVER (PARTITION BY DATE_FORMAT(`event_time`, '%Y-%m') ORDER BY SUM(price) DESC) AS rank_desc,
         RANK() OVER (PARTITION BY DATE_FORMAT(`event_time`, '%Y-%m') ORDER BY SUM(price) ASC) AS rank_asc
 	FROM `events_updated`
 	WHERE `event_type` = 'purchase'
 	GROUP BY `year-month`, `hour`
 ),
 cte_top_3 AS (
-	SELECT *
-	FROM cte_ranking
+    SELECT *
+    FROM cte_ranking
     WHERE rank_desc <= 3
 ),
 cte_bottom_3 AS (
-	SELECT *
-	FROM cte_ranking
+    SELECT *
+    FROM cte_ranking
     WHERE rank_asc <= 3
 )
 SELECT
-	t.`year-month` AS `year-month`,
-	t.`hour` AS `hour (top_ranking)`,
-	t.total_revenue AS total_revenue_top,
+    t.`year-month` AS `year-month`,
+    t.`hour` AS `hour (top_ranking)`,
+    t.total_revenue AS total_revenue_top,
     b.`hour` AS `hour (bottom_ranking)`,
-	b.total_revenue AS total_revenue_bottom
+    b.total_revenue AS total_revenue_bottom
 FROM
-	cte_top_3 t
-		JOIN
-	cte_bottom_3 b ON t.`year-month` = b.`year-month` AND t.rank_desc = b.rank_asc
+    cte_top_3 t
+        JOIN
+    cte_bottom_3 b ON t.`year-month` = b.`year-month` AND t.rank_desc = b.rank_asc
 ORDER BY t.`year-month`, t.rank_desc;
 
 -- Most purchase were done in day time, while midnight has the least purchase in every month.
@@ -90,35 +90,35 @@ ORDER BY t.`year-month`, t.rank_desc;
 -- Determine `event_type`
 
 SELECT
-	DISTINCT `event_type`
+    DISTINCT `event_type`
 FROM `events_updated`;
 
 -- There are 'view', 'cart' and 'puchase' event types.
 
 WITH cte_purchase AS (
-SELECT
-	`user_session`,
-	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `user_session`
+    SELECT
+        `user_session`,
+        SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `user_session`
 )
 SELECT
-	ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS conversion_rate
+    ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS conversion_rate
 FROM cte_purchase;
 
 -- Session-based conversion rate is 5%.
 
 WITH cte_purchase AS (
-SELECT
-	`user_id`,
-	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_id` IS NOT NULL
-GROUP BY `user_id`
+    SELECT
+        `user_id`,
+        SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
+    FROM `events_updated`
+    WHERE `user_id` IS NOT NULL
+    GROUP BY `user_id`
 )
 SELECT
-	ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_id`) * 100.0, 2) AS conversion_rate
+    ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_id`) * 100.0, 2) AS conversion_rate
 FROM cte_purchase;
 
 -- User-based conversion rate is 5.27%
@@ -126,17 +126,17 @@ FROM cte_purchase;
 -- Conversion rate by month
 
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
 	`user_session`,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `year-month`, `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `year-month`, `user_session`
 )
 SELECT
-	`year-month`,
-	ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS conversion_rate
+    `year-month`,
+    ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS conversion_rate
 FROM cte_purchase
 GROUP BY `year-month`;
 
@@ -145,13 +145,13 @@ GROUP BY `year-month`;
 -- 2.3. Average Order Value (AOV)
 
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	`user_session`,
-    SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
+        SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `user_session`
 )
 SELECT
     ROUND(SUM(revenue) / SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END), 2) AS average_order_value
@@ -162,17 +162,17 @@ FROM cte_purchase;
 -- Average Order Value (AOV) by month
 
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
 	`user_session`,
-    SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
+        SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `year-month`, `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `year-month`, `user_session`
 )
 SELECT
-	`year-month`,
+    `year-month`,
     ROUND(SUM(revenue) / SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END), 2) AS average_order_value
 FROM cte_purchase
 GROUP BY `year-month`;
@@ -182,16 +182,16 @@ GROUP BY `year-month`;
 -- 2.4. Cart Abandonment Rate (CAR)
 
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	`user_session`,
-    SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
+        SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `user_session`
 )
 SELECT
-	ROUND((1 - (SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / SUM(CASE WHEN num_cart > 0 THEN 1 ELSE 0 END))) * 100.0, 2) AS cart_abandonment_rate
+    ROUND((1 - (SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / SUM(CASE WHEN num_cart > 0 THEN 1 ELSE 0 END))) * 100.0, 2) AS cart_abandonment_rate
 FROM cte_purchase;
 
 -- Cart abondentment rate is 41.04%
@@ -199,18 +199,18 @@ FROM cte_purchase;
 -- Cart Abandonment Rate (CAR) by month
 
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	DATE_FORMAT(`event_time`, '%Y-%m') AS `year-month`,
 	`user_session`,
-    SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
+        SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `year-month`, `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `year-month`, `user_session`
 )
 SELECT
-	`year-month`,
-	ROUND((1 - (SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / SUM(CASE WHEN num_cart > 0 THEN 1 ELSE 0 END))) * 100.0, 2) AS cart_abandonment_rate
+    `year-month`,
+    ROUND((1 - (SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / SUM(CASE WHEN num_cart > 0 THEN 1 ELSE 0 END))) * 100.0, 2) AS cart_abandonment_rate
 FROM cte_purchase
 GROUP BY `year-month`;
 
@@ -219,7 +219,7 @@ GROUP BY `year-month`;
 -- 2.4. Create new table for total revenue, CR, AOV, CAR
 
 CREATE TABLE `kpi` (
-	`Year-Month` VARCHAR(10),
+    `Year-Month` VARCHAR(10),
     `Total Revenue` DOUBLE,
     `Conversion Rate` FLOAT,
     `Average Order Value` FLOAT,
@@ -228,20 +228,20 @@ CREATE TABLE `kpi` (
 
 INSERT INTO `kpi`
 WITH cte_purchase AS (
-SELECT
+    SELECT
 	DATE_FORMAT(`event_time`, '%Y-%m') AS `Year-Month`,
 	`user_session`,
-    SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
-    SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
+        SUM(CASE WHEN `event_type` = 'purchase' THEN `price` ELSE 0 END) AS revenue,
+        SUM(CASE WHEN `event_type` = 'cart' THEN 1 ELSE 0 END) AS num_cart,
 	SUM(CASE WHEN `event_type` = 'purchase' THEN 1 ELSE 0 END) AS num_purchase
-FROM `events_updated`
-WHERE `user_session` IS NOT NULL
-GROUP BY `Year-Month`, `user_session`
+    FROM `events_updated`
+    WHERE `user_session` IS NOT NULL
+    GROUP BY `Year-Month`, `user_session`
 )
 SELECT
-	`Year-Month`,
+    `Year-Month`,
     ROUND(SUM(revenue), 2) AS `Total Revenue`,
-	ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS `Conversion Rate`,
+    ROUND(SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / COUNT(`user_session`) * 100.0, 2) AS `Conversion Rate`,
     ROUND(SUM(revenue) / SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END), 2) AS `Average Order Value`,
     ROUND((1 - (SUM(CASE WHEN num_purchase > 0 THEN 1 ELSE 0 END) / SUM(CASE WHEN num_cart > 0 THEN 1 ELSE 0 END))) * 100.0, 2) AS `Cart Abandonment Rate`
 FROM cte_purchase
